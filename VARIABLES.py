271016363ea0ed5993171ab07652edd7b7bd10c4
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2024-04-29 19:50:39 -0400

    refactor(single-video-process): modularized and working properly

--- VARIABLES.py
+++ VARIABLES.py
@@ -1,14 +1,14 @@
 import os 
 
-ORIGINAL_VIDEO_NAME = "MOVI0016"
-FILE_TYPE = ".avi"
+ORIGINAL_VIDEO_NAME = "2"
+FILE_TYPE = ".mp4"
 SOURCE_ORIGINAL_VIDEO_PATH = os.path.join("original_videos", f"{ORIGINAL_VIDEO_NAME}{FILE_TYPE}")
 
 
 COUNTER = 2
 VIDEO_NAME = f"{ORIGINAL_VIDEO_NAME}-short{COUNTER}"
 SOURCE_VIDEO_PATH = os.path.join("video_shorts", f"{VIDEO_NAME}{FILE_TYPE}")
-COUNTER_OUTPUT = 7
+COUNTER_OUTPUT = 8
 TARGET_VIDEO_PATH = f"video_results/{VIDEO_NAME}-output{COUNTER_OUTPUT}{FILE_TYPE}"
 TARGET_DUMMY_VIDEO_PATH = f"video_results/dummy.avi"
 
