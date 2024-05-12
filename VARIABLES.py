e72136d3170b1621caab41a9ac1882f045676d15
Ignacio Mansuy <ignacio.mansuy@gmail.com>
2024-05-12 19:03:13 -0400

    refactor to line zones and testing with run 11 (better results but not sufficient)

--- VARIABLES.py
+++ VARIABLES.py
@@ -1,6 +1,8 @@
 import os 
 
-MODEL = "runs/detect/train10/weights/best.pt"
+# MODEL = "yolov8x.pt"
+# MODEL = "runs/detect/train10/weights/best.pt"
+MODEL = "runs/detect/train11/weights/best.pt"
 ORIGINAL_VIDEO_NAME = "MOVI0016"
 FILE_TYPE = ".avi"
 SOURCE_ORIGINAL_VIDEO_PATH = os.path.join("original_videos", f"{ORIGINAL_VIDEO_NAME}{FILE_TYPE}")
@@ -9,7 +11,7 @@ SOURCE_ORIGINAL_VIDEO_PATH = os.path.join("original_videos", f"{ORIGINAL_VIDEO_N
 COUNTER = 2
 VIDEO_NAME = f"{ORIGINAL_VIDEO_NAME}-short{COUNTER}"
 SOURCE_VIDEO_PATH = os.path.join("video_shorts", f"{VIDEO_NAME}{FILE_TYPE}")
-COUNTER_OUTPUT = 9
+COUNTER_OUTPUT = 10
 TARGET_VIDEO_PATH = f"video_results/{VIDEO_NAME}-output{COUNTER_OUTPUT}{FILE_TYPE}"
 TARGET_DUMMY_VIDEO_PATH = f"video_results/dummy.avi"
 
@@ -18,3 +20,7 @@ THICKNESS_DEFAULT = 4
 TEXT_THICKNESS_DEFAULT = 4
 TEXT_SCALE_DEFAULT = 2
 TRACE_LENGTH_DEFAULT = 4
+
+
+AMOUNT_LINE_ZONES = 8
+COVERAGE_LINE_ZONES = (0.25, 0.75)
--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ import numpy as np
 import torch
 from line_zone import LineZone
 import csv
+from ultralytics import YOLO
 
 
 
@@ -18,25 +19,24 @@ class VideoInfoHandler():
   def __init__(self) -> None:
     self.video_info = None
     self.va_params = {}
-    self.line_zone_annotators = []
+    self.bounding_box_annotator: sv.BoundingBoxAnnotator = None
     self.label_annotator = None
-    self.trace_annotator = None
     self.byte_tracker = None
     self.line_zones: List[LineZone] = [] 
     pass
     
   def re_init(self, SOURCE_VIDEO_PATH):
     self.video_info = sv.VideoInfo.from_video_path(SOURCE_VIDEO_PATH)
-    self.init_va_params()
-    self.init_line_zone_annotators()
+    self.__init_va_params()
+    self.bounding_box_annotator = sv.BoundingBoxAnnotator(thickness=1)
     self.label_annotator = sv.LabelAnnotator( text_thickness=self.va_params["text_thickness"], text_scale=self.va_params["text_scale"])
-    self.trace_annotator = sv.TraceAnnotator(thickness=self.va_params["thickness"], trace_length=self.va_params["trace_length"])
     self.byte_tracker = sv.ByteTrack(
       track_activation_threshold=0.25, lost_track_buffer=30, minimum_matching_threshold=0.8, frame_rate=self.video_info.fps
     )
-    self.init_line_zones()
+    self.__init_line_zone_annotators()
+    self.__init_line_zones()
     
-  def init_va_params(self):
+  def __init_va_params(self):
     video_default_size = calculate_hypotenuse(1920, 1080)
     video_current_size = calculate_hypotenuse(self.video_info.width, self.video_info.height)
     proportion = video_current_size / video_default_size
@@ -46,29 +46,16 @@ class VideoInfoHandler():
         "text_scale": TEXT_SCALE_DEFAULT * proportion,
         "trace_length": round(TRACE_LENGTH_DEFAULT * proportion),
     }
-    
-  def init_line_zone_annotators(self):
+  
+  def __init_line_zone_annotators(self):
     self.line_zone_annotators = [sv.LineZoneAnnotator(
-                thickness=self.va_params["thickness"],
-                text_thickness=self.va_params["text_thickness"],
-                text_scale=self.va_params["text_scale"]
-                )
-              for _ in range(3)]
-    
-  def get_line_zones(self) -> List[LineZone]:
-    line_zones = []
-    for i in [-1, 0, 1]:
-        x = self.video_info.width * (1 / 2 + i * 0.15)
-        line_zones.append(
-          LineZone(
-          start=sv.Point( x, 0),
-          end=sv.Point(x, self.video_info.height)
-          )
-        )
-    return line_zones
-          
-  def init_line_zones(self):
-    new_line_zones = self.get_line_zones()
+                thickness=1,
+                text_thickness=1,
+                text_scale=0.35)
+              for _ in range(AMOUNT_LINE_ZONES)]
+              
+  def __init_line_zones(self):
+    new_line_zones = self.__get_line_zones()
     if self.line_zones:
       for i, ex_line in enumerate(self.line_zones):
         new_line_zones[i].in_count = ex_line.in_count
@@ -79,12 +66,28 @@ class VideoInfoHandler():
           new_line_zones[i].class_out_count[key] = value
           
     self.line_zones = new_line_zones
-    
 
+  def __get_line_zones(self) -> List[LineZone]:
+    line_zones = []
+    for percentage in self.__divide_segment(*COVERAGE_LINE_ZONES, AMOUNT_LINE_ZONES):
+        x = int(self.video_info.width * percentage)
+        line_zones.append(
+          LineZone(
+          start=sv.Point(x, 0),
+          end=sv.Point(x, self.video_info.height)
+          )
+        )
+    return line_zones
+    
+  def __divide_segment(self, start, end, num_parts):
+    step = (end - start) / (num_parts - 1)
+    values = [start + i * step for i in range(num_parts)]
+    return values
             
 
 
-def callback(frame: np.ndarray, index:int, model, selected_classes, vih) -> np.ndarray:
+def callback(frame: np.ndarray, index:int, model: YOLO,
+             selected_classes: List[int], vih: VideoInfoHandler) -> np.ndarray:
     # model prediction on single frame and conversion to supervision Detections
     results = model(frame, verbose=False, device=torch.device("cuda:0"))[0]
     detections = sv.Detections.from_ultralytics(results)
@@ -97,21 +100,22 @@ def callback(frame: np.ndarray, index:int, model, selected_classes, vih) -> np.n
         for confidence, class_id, tracker_id
         in zip(detections.confidence, detections.class_id, detections.tracker_id)
     ]
-    annotated_frame = vih.trace_annotator.annotate(
-        scene=frame.copy(),
-        detections=detections
-    )
     annotated_frame=vih.label_annotator.annotate(
-        scene=annotated_frame,
+        scene=frame,
         detections=detections,
         labels=labels)
+    
+    annotated_frame=vih.bounding_box_annotator.annotate(
+      scene=annotated_frame,
+      detections=detections
+    )
 
-    # update line counter
     for line_zone in vih.line_zones:
         line_zone.trigger(detections)
-    # return frame with box and line annotated result
-    for i in range(3):
-        annotated_frame = vih.line_zone_annotators[i].annotate(annotated_frame, line_counter=vih.line_zones[i])
+      
+    for i, line_annotator in enumerate(vih.line_zone_annotators):
+        annotated_frame = line_annotator.annotate(annotated_frame, line_counter=vih.line_zones[i])
+
     return  annotated_frame
 
 def process_video(
@@ -153,7 +157,7 @@ def process_video(
     with sv.VideoSink(target_path=target_path, video_info=source_video_info) as sink:
         for index, frame in tqdm(enumerate(
             sv.get_video_frames_generator(source_path=source_path, stride=stride)
-        ), desc=" Video processing", position=1, leave=False, total=source_video_info.total_frames):
+        ), desc=" Video processing", position=1, leave=False, total=source_video_info.total_frames - 217):
             result_frame = callback(frame, index, model, selected_classes, vih)
             sink.write_frame(frame=result_frame)
 
